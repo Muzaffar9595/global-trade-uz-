2269dc1c6baa237e6df7aabbc51719ff3ad6a75c
Muzaffar9595 <mmuhammadiyev925@gmail.com>
2026-09-24 06:00:27 +0500

    Implement MainActivity with WebView loading URL

---
+++ app/src/main/java/uz/globaltrade/app/MainActivity.java
@@ -0,0 +1,23 @@
+package uz.globaltrade.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("https://muzaffar9595.github.io/global-trade-uz-/");
+    }
+}
